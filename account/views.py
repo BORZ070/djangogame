c2a9d946d5b05cdc7f957cb4229807fb0cbbb28d
BORZ070 <shdgit07@gmail.com>
2023-10-04 20:57:24 +0300

    update 1.1

--- account/views.py
+++ account/views.py
@@ -1,8 +1,30 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 from account.forms import LoginForm
 
 def login_views(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse('authenticate successful')
+            else:
+                return HttpResponse("user don't active")
+        else:
+            return HttpResponse("user don't found")
+    else:
+        form = LoginForm()
+
+
+
+
+
     return render(request, 'registration/login.html', {'form':form})
 
 
