57b2965a8f2858c73107a220c00cd6a7163db352
BORZ070 <shdgit07@gmail.com>
2023-10-06 21:14:18 +0300

    update 2

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,20 @@
+# from account.views import login_views
 from django.urls import path
-from account.views import login_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', login_views, name='login')
+    # login_path
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # password_change
+    path('password-change/',auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/',auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # password_reset
+    path('password-reset/',auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/',auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -3,26 +3,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from account.forms import LoginForm
 
-def login_views(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponse('authenticate successful')
-            else:
-                return HttpResponse("user don't active")
-        else:
-            return HttpResponse("user don't found")
-    else:
-        form = LoginForm()
-
-    return render(request, 'registration/login.html', {'form':form})
+# def login_views(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             if user.is_active:
+#                 login(request, user)
+#                 return HttpResponse('authenticate successful')
+#             else:
+#                 return HttpResponse("user don't active")
+#         else:
+#             return HttpResponse("user don't found")
+#     else:
+#         form = LoginForm()
+#
+#     return render(request, 'registration/login.html', {'form':form})
 
 
 def index_views(request):
-    return HttpResponse('index_page')
+    return render(request, 'index_page.html')
