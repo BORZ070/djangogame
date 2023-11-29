00108a4847dace35ced762ac8592664dce93a684
BORZ070 <shdgit07@gmail.com>
2023-11-29 21:16:07 +0300

    update(tg model)

--- tbot/bot.py
+++ tbot/bot.py
@@ -2,8 +2,9 @@ import telebot
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+#t.me/django_games_test_bot
 token = '6783897789:AAF0J26XI-tEseNmlhT0z0_cGYhC1Wxodzc'
-webhook = 'https://17f6-5-144-122-243.ngrok-free.app/tbot/webhook/'
+webhook = 'https://5890-5-144-117-45.ngrok-free.app/tbot/webhook/'
 bot = telebot.TeleBot(token)
 def set_webhook(request):
     webhook_url = webhook
@@ -23,6 +24,9 @@ def telegram_webhook(request):
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Hello')
-
+    id = message.chat.id
+    first_name = message.chat.first_name
+    username = message.chat.username
+    
 
 
--- tbot/models.py
+++ tbot/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
+from games.models import Genre
 
-# Create your models here.
+
+class TbotUserProfile(models.Model):
+    tuser_id = models.IntegerField()
+    tuser_name = models.CharField(max_length=200)
+    tuser_first_name = models.CharField(max_length=200)
+    tuser_preference_genre = models.ForeignKey(Genre, on_delete=models.PROTECT, blank=True, null=True)
+    tspam = models.BooleanField(default=True)
+    tuser_link_to_user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.tuser_name}, {self.tuser_link_to_user}'
