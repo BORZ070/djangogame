9d963e339483dce018646820343cbf6598d7305a
BORZ070 <shdgit07@gmail.com>
2023-12-11 21:17:25 +0300

    update(create link logic)

--- tbot/bot.py
+++ tbot/bot.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 
 from tbot.models import TbotUserProfile
+from tbot.views import tlink_views
 
 #t.me/django_games_test_bot
 token = settings.TGTOKEN
@@ -27,23 +28,54 @@ def telegram_webhook(request):
 @bot.message_handler(commands=['start'])
 def start(message):
     text = message.text
-    # django_user_id =
+    django_user_id = None
+    try:
+        message_text = text.split()
+        django_user_id = message_text[1]
+    except Exception as err:
+        print(err)
     id = message.chat.id
     first_name = message.chat.first_name
     username = message.chat.username
     #testcode
     user_exists = TbotUserProfile.objects.filter(tuser_id=id).exists()
     print(user_exists)
-    # if not user_exists:
-    try:
-        TbotUserProfile.objects.create(tuser_id=id,
-                                       tuser_name=username,
-                                       tuser_first_name=first_name,
-                                       )
-    except Exception as err:
-        print(err)
 
+    if django_user_id:
+        if user_exists:
+            tg_user = TbotUserProfile.objects.get(tuser_id=id)
+            link_to_user_exists = tg_user.tuser_link_to_user
+            if link_to_user_exists:
+                pass
+            else:
+                tg_user.tuser_link_to_user_id = django_user_id
+                tg_user.save()
+        else:
+            TbotUserProfile.objects.create(tuser_id=id,
+                                           tuser_name=username,
+                                           tuser_first_name=first_name,
+                                           tuser_link_to_user_id=django_user_id,
+                                           )
+    else:
+        if user_exists:
+            pass
+        else:
+            TbotUserProfile.objects.create(tuser_id=id,
+                                           tuser_name=username,
+                                           tuser_first_name=first_name,
+                                           )
+    # if not user_exists:
+    # try:
+    #     TbotUserProfile.objects.create(tuser_id=id,
+    #                                    tuser_name=username,
+    #                                    tuser_first_name=first_name,
+    #                                    tuser_link_to_user_id=django_user_id,
+    #                                    )
+    # except Exception as err:
+    #     print(err)
+    #
     bot.send_message(message.chat.id, f'Hello, {username}')
+    print(django_user_id)
 
 def send_tmessage(chat_id, text):
     bot.send_message(chat_id, text)
--- tbot/views.py
+++ tbot/views.py
@@ -3,7 +3,7 @@ from django.conf import settings
 def tlink_views(request):
     base_link = settings.TGLINK
     with_id_link = settings.TGLINK
-    user_id  = request.user.id
+    user_id = request.user.id
     with_id_link += f'?start={user_id}'
     print(with_id_link)
 
