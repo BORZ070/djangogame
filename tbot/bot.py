bd3c6a7dca8a4c2ae7549f2ea4ddb1f012dabcac
BORZ070 <shdgit07@gmail.com>
2023-12-13 21:15:05 +0300

    update(create button)

--- tbot/bot.py
+++ tbot/bot.py
@@ -5,6 +5,9 @@ from django.conf import settings
 
 from tbot.models import TbotUserProfile
 from tbot.views import tlink_views
+from telebot import types
+
+from articles.models import Article
 
 #t.me/django_games_test_bot
 token = settings.TGTOKEN
@@ -64,18 +67,45 @@ def start(message):
                                            tuser_name=username,
                                            tuser_first_name=first_name,
                                            )
-    # if not user_exists:
-    # try:
-    #     TbotUserProfile.objects.create(tuser_id=id,
-    #                                    tuser_name=username,
-    #                                    tuser_first_name=first_name,
-    #                                    tuser_link_to_user_id=django_user_id,
-    #                                    )
-    # except Exception as err:
-    #     print(err)
-    #
-    bot.send_message(message.chat.id, f'Hello, {username}')
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item_1 = types.KeyboardButton('articles')
+    item_2 = types.KeyboardButton('games')
+    item_3 = types.KeyboardButton('library')
+    item_4 = types.KeyboardButton('profile')
+    markup.row(item_1, item_2)
+    markup.row(item_3, item_4)
+
+    bot.send_message(message.chat.id, f'Hello, {username}', reply_markup=markup)
     print(django_user_id)
+
+
+@bot.message_handler(func=lambda message: message.text == 'articles')
+def item_1(message):
+    articles = Article.objects.all()
+    for article in articles:
+        answer = f'{article.title}\nhttp://127.0.0.1:8000/{article.get_absolute_url()}'
+        bot.send_message(message.chat.id, answer)
+
+
+
+@bot.message_handler(func=lambda message: message.text == 'games')
+def item_2(message):
+    answer = 'you press button games'
+    bot.send_message(message.chat.id, answer)
+
+
+@bot.message_handler(func=lambda message: message.text == 'library')
+def item_3(message):
+    answer = 'you press button library'
+    bot.send_message(message.chat.id, answer)
+
+
+@bot.message_handler(func=lambda message: message.text == 'profile')
+def item_4(message):
+    answer = 'you press button profile'
+    bot.send_message(message.chat.id, answer)
+
 
 def send_tmessage(chat_id, text):
     bot.send_message(chat_id, text)
